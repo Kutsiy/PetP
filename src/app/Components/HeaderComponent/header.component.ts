d7768be70805bfd83906d8da4ab2b4df2af2410d
Egor Kutsiy <kycuj.egor2020@gmail.com>
2024-07-18 15:45:04 +0300

    header 1.1

--- src/app/Components/HeaderComponent/header.component.ts
+++ src/app/Components/HeaderComponent/header.component.ts
@@ -1,28 +1,51 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
-import { platform } from 'os';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMenuClose = true;
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(PLATFORM_ID) private platformId: Object,
     private elementRef: ElementRef
   ) {}
+
   themeToggle() {
     if (isPlatformBrowser(this.platformId)) {
       this.document.body.dataset['theme'] =
         this.document.body.dataset['theme'] === 'light' ? 'dark' : 'light';
+      localStorage.setItem('theme', this.document.body.dataset['theme']);
     }
   }
 
   mobileMenuToggle() {
-    this.elementRef.nativeElement
-      .querySelector('.header__mobile-menu')
-      .classList.toggle('show');
+    if (isPlatformBrowser(this.platformId)) {
+      this.elementRef.nativeElement
+        .querySelector('.header__mobile-menu')
+        .classList.toggle('show');
+      this.isMenuClose = this.isMenuClose === true ? false : true;
+      this.document.body.style.overflow =
+        this.document.body.style.overflow === 'hidden' ? '' : 'hidden';
+    }
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      let localTheme = localStorage.getItem('theme');
+      if (localTheme) {
+        this.document.body.dataset['theme'] = localTheme;
+      }
+    }
   }
 }
