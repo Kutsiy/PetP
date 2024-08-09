5bab7cff2fbf440a9c590be83270eee0449cb443
Egor Kutsiy <kycuj.egor2020@gmail.com>
2024-08-09 11:19:58 +0300

    feat: added a footer

--- src/app/Components/UI/card/card.component.ts
+++ src/app/Components/UI/card/card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  @Input() title: string | undefined;
+  @Input() image: string | undefined;
+  @Input() link: string | undefined;
 }
