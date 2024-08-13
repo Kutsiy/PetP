7cc5e7d9fdae164be2a6c0999e8ab93f2eba643b
Egor Kutsiy <kycuj.egor2020@gmail.com>
2024-08-13 20:55:46 +0300

    feat: added new feature to posts component #2

--- src/app/Components/UI/post/post.component.ts
+++ src/app/Components/UI/post/post.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
 export class PostComponent {
-
+  @Input() post: any = null;
 }
--- src/app/Components/UI/posts/posts.component.ts
+++ src/app/Components/UI/posts/posts.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss'
+  styleUrl: './posts.component.scss',
 })
-export class PostsComponent {
-
+export class PostsComponent implements OnInit {
+  data: any = null;
+  constructor(@Inject(HttpClient) private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((data) => {
+        this.data = data;
+      });
+  }
 }
