0978590193e7b9a2ddb2d2fd99746876c2cfe2a7
Egor Kutsiy <kycuj.egor2020@gmail.com>
2024-08-28 20:32:45 +0300

    feat: added initial logic to post.service

--- src/app/Services/posts.service.ts
+++ src/app/Services/posts.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+
+const GET_POSTS = gql`
+  query GetPosts {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
+  loading: boolean = false;
+  posts: any;
 
-  constructor() { }
+  private querySubscription: Subscription | undefined = undefined;
+
+  constructor(private readonly apollo: Apollo) {}
+
+  start() {
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: GET_POSTS,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.posts = data.posts;
+      });
+  }
+  stop() {
+    this.querySubscription?.unsubscribe();
+  }
 }
