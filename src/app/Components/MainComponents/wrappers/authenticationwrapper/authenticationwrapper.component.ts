20b478cfab25545114708cd94268857b4c7e20cc
Egor Kutsiy <kycuj.egor2020@gmail.com>
2024-11-12 22:36:29 +0200

    feat: Added some style for auth component

--- src/app/Components/MainComponents/wrappers/authenticationwrapper/authenticationwrapper.component.ts
+++ src/app/Components/MainComponents/wrappers/authenticationwrapper/authenticationwrapper.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-authenticationwrapper',
@@ -7,14 +8,71 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 })
 export class AuthenticationwrapperComponent implements OnInit {
   element: HTMLDivElement | null = null;
-  constructor(private elementRef: ElementRef) {}
+  elementLogin: HTMLDivElement | null = null;
+  elementSignUp: HTMLDivElement | null = null;
+  isLogin: boolean = true;
+  addStyle: boolean = false;
+  authType: string | null = null;
+  constructor(
+    private elementRef: ElementRef,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
   ngOnInit(): void {
     this.element = this.elementRef.nativeElement.querySelector(
       '.auth__box'
     ) as HTMLDivElement;
+    this.elementLogin = this.elementRef.nativeElement.querySelector(
+      '.auth__login'
+    ) as HTMLDivElement;
+    this.elementSignUp = this.elementRef.nativeElement.querySelector(
+      '.auth__sign-up'
+    ) as HTMLDivElement;
+    this.route.queryParams.subscribe((params) => {
+      if (this.element) {
+        this.authType = params['authType'];
+        if (
+          params['authType'] === undefined ||
+          (params['authType'] !== 'login' && params['authType'] !== 'sign_up')
+        ) {
+          this.router.navigate([], {
+            queryParams: { authType: 'login' },
+          });
+        }
+        if (params['authType'] === 'login') {
+          this.element.classList.remove('active');
+        } else if (params['authType'] === 'sign_up') {
+          this.element.classList.add('active');
+        }
+      }
+    });
+  }
+
+  styiles() {
+    if (this.element && this.elementLogin && this.elementSignUp) {
+      this.element.classList.add('auth__time');
+      this.elementLogin.classList.add('auth__time');
+      this.elementSignUp.classList.add('auth__time');
+    }
+    this.addStyle = true;
   }
 
   activate() {
-    if (this.element) this.element.classList.toggle('active');
+    if (this.authType === 'login') {
+      this.router.navigate([], {
+        queryParams: { authType: 'sign_up' },
+      });
+    } else if (this.authType === 'sign_up') {
+      this.router.navigate([], {
+        queryParams: { authType: 'login' },
+      });
+    }
+    if (!this.addStyle) {
+      this.styiles();
+    }
+    if (this.element && this.isLogin) {
+      this.element.classList.toggle('active');
+    }
   }
 }
