232bc417e60811d46f9e4bd92620aa2642a1ae07
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-02-22 15:36:04 +0200

    feat: created not-auth page component

---
+++ src/pages/not-auth/not-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-auth',
+  standalone: false,
+
+  templateUrl: './not-auth.component.html',
+  styleUrl: './not-auth.component.scss',
+})
+export class NotAuthPageComponent {}
