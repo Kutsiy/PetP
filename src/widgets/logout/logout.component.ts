f8ab94bc6786e37f2274d1067450e40dae84d9fa
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-03-01 18:19:59 +0200

    feat: added logout component

---
+++ src/widgets/logout/logout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  standalone: false,
+
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss',
+})
+export class LogOutWidgetComponent {}
---
+++ src/widgets/logout/logout.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { LogOutWidgetComponent } from './logout.component';
+
+@NgModule({
+  declarations: [LogOutWidgetComponent],
+  imports: [],
+  exports: [LogOutWidgetComponent],
+})
+export class LogOutWidgetModule {}
