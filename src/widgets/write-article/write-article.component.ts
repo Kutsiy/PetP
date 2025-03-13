00a8baec2ad2b594d7946e42afba11e7ee6a989c
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-03-13 19:47:02 +0200

    feat: added style for file input in write-article

--- src/widgets/write-article/write-article.component.ts
+++ src/widgets/write-article/write-article.component.ts
@@ -12,4 +12,17 @@ export class WriteArticleWidgetComponent {
     { value: 'About me', viewValue: 'About me' },
     { value: 'News about programming', viewValue: 'News about programming' },
   ];
+
+  imageUrl: string | null = null;
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imageUrl = e.target?.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
