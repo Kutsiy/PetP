edca011aed6ecec387ddab15d350a09a1f79c00d
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-03-24 19:25:19 +0200

    feat: some style changes

--- src/widgets/main/main.module.ts
+++ src/widgets/main/main.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MainWidgetComponent } from './main.component';
-import { CubeUiModule } from '../../shared/ui';
+import { BaseButtonUiModule, CubeUiModule } from '../../shared/ui';
 
 @NgModule({
   declarations: [MainWidgetComponent],
-  imports: [RouterModule, MatIconModule, CubeUiModule],
+  imports: [RouterModule, MatIconModule, CubeUiModule, BaseButtonUiModule],
   exports: [MainWidgetComponent],
 })
 export class MainWidgetModule {}
