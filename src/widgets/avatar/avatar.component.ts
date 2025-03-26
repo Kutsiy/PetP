7d90c826aed57a24b1d1756a18994f13f40d0d0c
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-03-26 20:39:31 +0200

    feat: added some change to avatar component

--- src/widgets/avatar/avatar.component.ts
+++ src/widgets/avatar/avatar.component.ts
@@ -26,6 +26,7 @@ export class AvatarWidgetComponent implements OnInit {
   openCropper = false;
   imageChangedEvent: any = null;
   croppedImage: any = '';
+  imageBlob: BlobPart | null | undefined = null;
 
   onFileSelected(event: any): void {
     this.imageChangedEvent = event;
@@ -34,10 +35,16 @@ export class AvatarWidgetComponent implements OnInit {
 
   imageCropped(event: ImageCroppedEvent) {
     this.croppedImage = event.objectUrl;
+    this.imageBlob = event.blob;
   }
 
   cropImage = () => {
+    if (!this.imageBlob) return;
     this.imageSrc = this.croppedImage;
+    const uniqueFileName = `avatar-${Date.now()}.png`;
+    const file = new File([this.imageBlob], uniqueFileName, {
+      type: 'image/png',
+    });
     this.store.dispatch(AuthActions.authSetAvatar({ avatar: this.imageSrc }));
     this.openCropper = false;
     this.imageChangedEvent.target.value = null;
