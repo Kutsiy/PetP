3e72956e73b1675b7e04a905102a5509cbf4494a
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-06-06 19:13:51 +0300

    feat: popular-articles change

--- src/widgets/post/post.module.ts
+++ src/widgets/post/post.module.ts
@@ -3,10 +3,11 @@ import { PostWidgetComponent } from './post.component';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { DatePipeModule } from '../../shared/pipes';
+import { SlicePipe } from '@angular/common';
 
 @NgModule({
   declarations: [PostWidgetComponent],
-  imports: [MatIconModule, RouterModule, DatePipeModule],
+  imports: [MatIconModule, RouterModule, DatePipeModule, SlicePipe],
   exports: [PostWidgetComponent],
 })
 export class PostWidgetModule {}
